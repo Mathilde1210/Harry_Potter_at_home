67812a0d15c3fef3e23f88945ac176ea17fb3eee
Mathilde1210 <117743506+Mathilde1210@users.noreply.github.com>
2023-03-10 14:36:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans Harry Potter at Home, le RPG des sorciers. Avez-vous l’étoffe des plus grands sorciers ? Saurez-vous relever tous les défis qui vous attendent ? C’est parti !\n" +
+                "Votre objectif est de valider votre scolarité à Poudlard, mais attention chaque année sera parsemée d’embuches.\n");
+    }
+}
