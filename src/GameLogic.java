f39c85a1a7a13553e90c23bbc8a545216a551c4e
Mathilde1210 <117743506+Mathilde1210@users.noreply.github.com>
2023-03-14 08:32:22 +0100

    création de game logic et test

---
+++ src/GameLogic.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class GameLogic {
+    static Scanner scanner = new Scanner(System.in);
+    //method to get user input from console
+    public static int reading (String prompt, int userChoices){
+        int input;
+        do{
+            System.out.println(prompt);
+            try{
+                input = Integer.parseInt(scanner.next());
+            }catch(Exception e){
+                input = -1;
+                System.out.println("Please enter an integer!");
+            }
+        }while(input < 1 || input > userChoices);
+        return input;
+    }
+    //method to stimulate clearing out the console
+    public static void clearConsole(){
+        for(int i = 0; i < 100; i++)
+            System.out.println();
+    }
+    //method to print a seperator with length n
+    public static void printSeperator(int n){
+        for(int i = 0; i < n; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+    //method to print a heading
+    public static void printHeading(String title){
+        printSeperator(30);
+        System.out.println(title);
+        printSeperator(30);
+    }
+    //method to stop the game until user enters anything
+    public static void anythingToContinue(){
+        System.out.println("\nEnter anything to continue...");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans Harry Potter at Home, le RPG des sorciers. Avez-vous l’étoffe des plus grands sorciers ? Saurez-vous relever tous les défis qui vous attendent ? C’est parti !\n" +
                 "Votre objectif est de valider votre scolarité à Poudlard, mais attention chaque année sera parsemée d’embuches.\n");
+        GameLogic.printHeading("Testing helper methods!");
+        GameLogic.anythingToContinue();
+        GameLogic.clearConsole();
+        int input = GameLogic.reading("Enter 1, 2 or 3;", 3);
+        System.out.println("You chose number " + input);
     }
 }
